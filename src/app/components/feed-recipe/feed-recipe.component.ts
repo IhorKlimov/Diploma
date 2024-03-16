2819ff1c5424cd52c4f5b4663d24af494d335cb9
Ihor Klimov <iklimov6@gmail.com>
2024-03-16 15:08:10 -0400

    Update my recipes with edit button

--- src/app/components/feed-recipe/feed-recipe.component.ts
+++ src/app/components/feed-recipe/feed-recipe.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe';
 import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { NgIf } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-feed-recipe',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonModule, NgIf, RippleModule,],
   templateUrl: './feed-recipe.component.html',
   styleUrl: './feed-recipe.component.css'
 })
@@ -13,4 +16,6 @@ export class FeedRecipeComponent {
   @Input()
   recipe!: Recipe;
 
+  @Input()
+  showEdit: boolean = false;
 }
